feebfdfd217d8164336b373d70d535777d545b99
Simon Perdrizat <simon.perdrizat@students.bfh.ch>
2022-10-18 06:18:22 +0200

    first commit

---
+++ Contact_files/_buildManifest.js
@@ -0,0 +1,28 @@
+(self.__BUILD_MANIFEST = (function (a) {
+  return {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    "/": ["static/chunks/pages/index-f8b1e010031dec53.js"],
+    "/_error": ["static/chunks/pages/_error-7397496ca01950b1.js"],
+    "/about": ["static/chunks/pages/about-8af717527306a5f6.js"],
+    "/contact": [
+      a,
+      "static/chunks/39-2ad5417b0090a61e.js",
+      "static/chunks/pages/contact-7635bddceb49ea3b.js",
+    ],
+    "/experience": ["static/chunks/pages/experience-48fd12034aa238cb.js"],
+    "/stackoverflow": [
+      a,
+      "static/chunks/pages/stackoverflow-03ed362d2abf7c2f.js",
+    ],
+    sortedPages: [
+      "/",
+      "/_app",
+      "/_error",
+      "/about",
+      "/contact",
+      "/experience",
+      "/stackoverflow",
+    ],
+  };
+})("static/chunks/7f0c75c1-e4a87f4c14df49a2.js")),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
